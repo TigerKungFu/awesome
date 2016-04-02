f4213069e71786401231b31dccb3e3386a3ff524
zhouxiuhu <zhouxiuhu@gmail.com>
2016-04-02 23:24:02 +0800

    Add TestSnip

---
+++ Test/TestSnip.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhouxiuhu
+ * Date: 3/23/16
+ * Time: 11:00 PM
+ */
+$a1 = array(1, 3, 5, 6);
+$a2 = array(2, 3, 6, 9);
+print_r(array_merge($a1, $a2));
+?>
